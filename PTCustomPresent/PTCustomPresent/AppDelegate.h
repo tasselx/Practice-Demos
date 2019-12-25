9aed95244b3536aa9adb15893c3b0b74824d97b8
tassel <liya@pita.live>
2019-12-25 15:44:46 +0800

    new

---
+++ PTCustomPresent/PTCustomPresent/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PTCustomPresent
+//
+//  Created by tassel on 2019/12/23.
+//  Copyright © 2019 tassel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@property (nonatomic, strong) UIWindow *window;
+
+@end
+
---
+++ PTCustomPresent/PTCustomPresent/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PTCustomPresent
+//
+//  Created by tassel on 2019/12/23.
+//  Copyright © 2019 tassel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
