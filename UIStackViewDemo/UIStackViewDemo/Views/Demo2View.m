c629633f65267bae70ad9636ad1dd7a43093888b
tassel <liya@pita.live>
2020-04-20 17:22:33 +0800

    stackview demo 更新

--- UIStackViewDemo/UIStackViewDemo/Views/Demo2View.m
+++ UIStackViewDemo/UIStackViewDemo/Views/Demo2View.m
@@ -36,7 +36,8 @@
     UIStackView *bottomStackView = [UIStackView new];
     bottomStackView.axis = UILayoutConstraintAxisVertical;
     bottomStackView.alignment = UIStackViewAlignmentFill;
-    bottomStackView.distribution = UIStackViewDistributionFillEqually;
+    bottomStackView.distribution = UIStackViewDistributionFill;
+    bottomStackView.spacing = 10;
     [self addSubview:bottomStackView];
 
     [topStackView mas_makeConstraints:^(MASConstraintMaker *make) {
@@ -44,10 +45,86 @@
         make.height.mas_equalTo(self).multipliedBy(0.7);
     }];
     [bottomStackView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.leading.trailing.bottom.mas_equalTo(self);
         make.top.mas_equalTo(topStackView.mas_bottom).offset(10);
+        make.leading.mas_equalTo(self).offset(10);
+        make.trailing.mas_equalTo(self).offset(-10);
     }];
 
+    {
+       //bottom subviews
+        UIStackView *headStackView = [UIStackView new];
+        headStackView.axis = UILayoutConstraintAxisHorizontal;
+        headStackView.spacing = 50;
+        headStackView.distribution = UIStackViewDistributionFillEqually;
+        headStackView.alignment = UIStackViewAlignmentTop;
+        
+        {
+            UILabel *textLabel = [UILabel new];
+            textLabel.text = @"一只刚会走的宓珂璟";
+            textLabel.textColor = UIColor.redColor;
+            textLabel.numberOfLines = 0;
+            textLabel.font = [UIFont systemFontOfSize:15 weight:UIFontWeightSemibold];
+            
+            UIStackView *headSubStackView = [UIStackView new];
+            headSubStackView.alignment = UIStackViewAlignmentFill;
+            headSubStackView.distribution = UIStackViewDistributionFillEqually;
+            headSubStackView.axis = UILayoutConstraintAxisHorizontal;
+            headSubStackView.spacing = 30;
+            
+            UIButton *likeBtn = [UIButton buttonWithType:UIButtonTypeSystem];
+            [likeBtn setTitle:@"点我喜欢" forState:UIControlStateNormal];
+            
+            UIButton *disLikeBtn = [UIButton buttonWithType:UIButtonTypeSystem];
+            [disLikeBtn setTitle:@"我不喜欢" forState:UIControlStateNormal];
+            
+            [headSubStackView addArrangedSubview:likeBtn];
+            [headSubStackView addArrangedSubview:disLikeBtn];
+            
+            [headStackView addArrangedSubview:textLabel];
+            [headStackView addArrangedSubview:headSubStackView];
+        }
+        
+        
+        UILabel *desLabel = [UILabel new];
+        desLabel.text = @"落霞与孤鹜齐飞，秋水共长天一色。渔舟唱晚，响穷彭蠡之滨，雁阵惊寒，声断衡阳之浦";
+        desLabel.numberOfLines = 0;
+        desLabel.textColor = [UIColor orangeColor];
+        desLabel.font = [UIFont systemFontOfSize:15 weight:UIFontWeightMedium];
+        desLabel.textAlignment = NSTextAlignmentCenter;
+        
+        UIStackView *tailStackView = [UIStackView new];
+        tailStackView.axis = UILayoutConstraintAxisHorizontal;
+        tailStackView.spacing = 20;
+        tailStackView.distribution = UIStackViewDistributionFillEqually;
+        tailStackView.alignment = UIStackViewAlignmentFill;
+        {
+            UIButton *leftButton = [UIButton buttonWithType:UIButtonTypeSystem];
+            leftButton.backgroundColor = [UIColor yellowColor];
+            [leftButton setTitle:@"Left" forState:UIControlStateNormal];
+            
+            UIButton *centerButton = [UIButton buttonWithType:UIButtonTypeSystem];
+            centerButton.backgroundColor = [UIColor redColor];
+            [centerButton setTitle:@"Center" forState:UIControlStateNormal];
+            
+            UIButton *rightButton = [UIButton buttonWithType:UIButtonTypeSystem];
+            rightButton.backgroundColor = [UIColor blueColor];
+            [rightButton setTitle:@"Right" forState:UIControlStateNormal];
+            
+            
+            [tailStackView addArrangedSubview:leftButton];
+            [tailStackView addArrangedSubview:centerButton];
+            [tailStackView addArrangedSubview:rightButton];
+        }
+        
+
+        [bottomStackView addArrangedSubview:headStackView];
+        [bottomStackView addArrangedSubview:desLabel];
+        [bottomStackView addArrangedSubview:tailStackView];
+        
+    
+
+    }
+    
 
 }
 
